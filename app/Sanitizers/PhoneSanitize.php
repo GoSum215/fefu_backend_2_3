5b027ac66b300857ae936a93d4e5963fa1070ef1
GoSum215 <kryukov.nv@students.dvfu.ru>
2021-12-16 00:13:04 +1000

    Lesson 6: Validate and sanitize appeal form input

---
+++ app/Sanitizers/PhoneSanitize.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Sanitizers;
+
+use function preg_replace;
+
+class PhoneSanitize
+{
+    public static function sanitize(?string $value) : ?string
+    {
+        if ($value === null) {
+            return null;
+        }
+        $phone = preg_replace('/\D+/', '', $value);
+        if ($phone[0] === '8') {
+            $phone[0] = '7';
+        }
+        return $phone;
+    }
+}
