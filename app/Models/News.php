b424783c9c533285ea9f97f5b3e91bc3113b8671
GoSum215 <85661196+GoSum215@users.noreply.github.com>
2021-12-23 12:32:04 +1000

    Lesson 7: Add redirecting middleware

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $title
+ * @property string $slug
+ * @property string $description
+ * @property string $text
+ * @property boolean $is_published
+ * @property Carbon $published_at
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 
 class News extends Model
 {
@@ -16,5 +28,20 @@ class News extends Model
                 'source' => 'title'
             ]
         ];
+    }
+
+    public function save(array $options = [])
+    {
+        if ($this->exists && $this->isDirty('slug'))
+        {
+           $oldSlug = $this->getOriginal('slug');
+           $newSlug = $this->slug;
+
+           $redirect = new Redirect();
+           $redirect->old_slug = route('news_item', ['slug' => $oldSlug], false);
+           $redirect->new_slug = route('news_item', ['slug' => $newSlug], false);
+           $redirect->save();
+        }
+        return parent::save($options);
     }
 }
